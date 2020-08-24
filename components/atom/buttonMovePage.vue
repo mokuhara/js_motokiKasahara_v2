2602772cbd8f85dc1ad823051d05fd00a62a65e6
MotokiKasaharaBansuke <motoki.kasahara@plaid.co.jp>
2020-08-24 09:33:33 +0900

    遷移ボタン作成

---
+++ components/atom/buttonMovePage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="container">
+      <button @click="movePage" :style="{'--color': this.btnColor}">>{{ btnText }}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+      pageURL: String,
+      btnText: String,
+      btnColor: String
+  },
+  methods: {
+    movePage () {
+      this.$router.push(this.pageURL)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+    --color:rgba($color: #000000, $alpha: 0.1);
+}
+
+.selectBtn {
+    padding: 10px 20px;
+    border-radius: 3px;
+    background: var(--color);
+}
+</style>
